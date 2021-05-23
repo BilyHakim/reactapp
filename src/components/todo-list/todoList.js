2e8c9c49fdcf65d7789851d129ce526050e72242
BilyHakim <bilyhakim1@gmail.com>
2021-05-23 19:45:40 +0700

    Melengkapi file yang ada

---
+++ src/components/todo-list/todoList.js
@@ -0,0 +1,14 @@
+import './todoList.css'
+
+const TodoList = (props) => {
+    console.log(props.dataTodos)
+    return (
+        <ul>{
+            props.dataTodos.map((todo) => {
+                return <li key={todo.id}>{todo.title}</li>
+            })
+        }</ul>
+    )
+}
+
+export default TodoList
